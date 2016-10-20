2107d00f1ce27cd2951ea6b7a4557476c010a308
jbrown102 <jbrown102@students.towson.edu>
2016-10-20 11:29:05 -0400

    Updated Compiler.scala and started filling out CONSTANTS.scala

--- src/main/scala/edu/towson/cosc/cosc455_jbrown_project1/CONSTANTS.scala
+++ src/main/scala/edu/towson/cosc/cosc455_jbrown_project1/CONSTANTS.scala
@@ -1,5 +1,13 @@
 package edu.towson.cosc.cosc455_jbrown_project1
 
 object CONSTANTS {
-  val DOCB : String = "\BEGIN"
+  val DOCB : String = "\\BEGIN"
+  val DOCE : String = "\\END"
+  val TITLEB : String = "\\TITLE["
+  val BRACKETE : String = "]"
+  val HEADING : String = "#"
+  val PARAB : String = "\\PARAB"
+  val PARAE : String = "\\PARAE"
+  val BOLD : String = "**"
+  val ITALICS : String = "*"
 }
--- src/main/scala/edu/towson/cosc/cosc455_jbrown_project1/Compiler.scala
+++ src/main/scala/edu/towson/cosc/cosc455_jbrown_project1/Compiler.scala
@@ -1,30 +1,31 @@
 package edu.towson.cosc.cosc455_jbrown_project1
 
+// import scala.io.Source._
+
 object Compiler {
 
-  var fileContents : String = ""
+  var currentToken : String = ""  // Space or no space
+  var fileContents : String = ""  // Same here
+
+  val Scanner = new MyLexicalAnalyzer
+  val Parser = new MySyntaxAnalyzer
+  val SemanticAnalyzer = new MySyntaxAnalyzer
 
   def main(args : Array[String]) = {
 
-    // println(args{0})
+    // println(args{0})  <- USE THIS??
+    checkfile(args)  // check usage
+    readfile(args(0))
+    // println(fileContents)  <- USE THIS??
+    Scanner.getNextToken()  // gets first token
 
-    // check usage
-    checkfile(args)
-    readfile(args {0})
-    println(fileContents)
-
-    // gets first token
-    Scanner.getNextToken()
-
-    // calls start state of BNF in SyntaxAnalyzer
-    Parser.gittex()
-
+    //  Parser.gittex()  // calls start state of BNF in SyntaxAnalyzer  <- USE THIS??
     // on return, there is a parse tree
   }
 
     def readfile(file : String) = {
-      val source = scala.io.Source.fromfile(file)
-      fileContents = try source.myString finally source.clone()
+      val source = scala.io.Source.fromFile(file)
+      fileContents = try source.mkString finally source.clone()
     }
 
     def checkfile(args : Array[String]) = {
